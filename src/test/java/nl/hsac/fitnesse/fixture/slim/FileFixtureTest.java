ef34c4194cbf5d6b928fb1cc5d5830478d49e55b
Johan Roeleveld <jhroeleveld@hotmail.com>
2018-11-19 09:14:39 +0100

    Refactoring delete (if exists)  and get/setDirectory tests

--- src/test/java/nl/hsac/fitnesse/fixture/slim/FileFixtureTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/slim/FileFixtureTest.java
@@ -2,14 +2,16 @@ package nl.hsac.fitnesse.fixture.slim;
 
 import nl.hsac.fitnesse.fixture.Environment;
 import nl.hsac.fitnesse.fixture.util.LineEndingHelper;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Paths;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -17,17 +19,30 @@ public class FileFixtureTest {
     private final FileFixture fixture = new FileFixture();
     private final String txtFilename = "testFileFixture.txt";
     private final String copyFilename = "temp-copy.txt";
+    private final String deleteFilename = "delete-me.txt";
     private final String curDir = Paths.get("").toAbsolutePath().toString();
     private final String testResourcesDir = curDir + "/src/test/resources/".replace('/', File.separatorChar);
 
+
+    private String getTmpFolderPath() {
+        return tmpFolder.getRoot() + File.separator;
+    }
+
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
+
     @Test
-    public void testGetAndSetDirectory() {
+    public void testGetDirectory() {
         String defaultFilesDir = Environment.getInstance().getFitNesseFilesSectionDir();
         String defaultFixtureDir = defaultFilesDir + File.separator + "fileFixture" + File.separator;
-        assertEquals(defaultFixtureDir, fixture.getDirectory());
 
-        fixture.setDirectory(testResourcesDir);
-        assertEquals(testResourcesDir, fixture.getDirectory());
+        assertThat(defaultFixtureDir).isEqualTo(fixture.getDirectory());
+    }
+
+    @Test
+    public void testSetAndGetDirectory() {
+        fixture.setDirectory(getTmpFolderPath());
+        assertThat(getTmpFolderPath()).isEqualTo(fixture.getDirectory());
     }
 
     @Test
@@ -40,18 +55,11 @@ public class FileFixtureTest {
     }
 
     @Test
-    public void testExists() {
-        fixture.setDirectory(testResourcesDir);
-        assertTrue(fixture.exists(txtFilename));
-        assertFalse(fixture.exists("foobar"));
-    }
-
-    @Test
     public void testTextIn() {
         try {
             fixture.setDirectory(testResourcesDir);
             assertEquals("# Expected as first line of text.\n# Expected as 2nd line of text.",
-                    fixture.textIn(txtFilename));
+                    new LineEndingHelper().convertEndingsTo(fixture.textIn(txtFilename), "\n"));
             fixture.textIn("foobar");
         } catch (SlimFixtureException sfe) {
             assertEquals("message:<<Unable to find: " + testResourcesDir + "foobar>>", sfe.getMessage());
@@ -59,15 +67,35 @@ public class FileFixtureTest {
     }
 
     @Test
-    public void testDeleteIfExists() {
-        try {
-            fixture.setDirectory(testResourcesDir);
-            if (fixture.exists(copyFilename)) {
-                fixture.deleteIfExists(copyFilename);
-            }
-        } catch (SlimFixtureException sfe) {
-            fail("Should not happen: " + sfe.getMessage());
-        }
+    public void testDeleteIfExists_non_existing_returns_false() {
+        String nonExistingFilePath = getTmpFolderPath() + "i-do-not-exist.lol";
+
+        Boolean result = fixture.deleteIfExists(nonExistingFilePath);
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testDeleteIfExists_existing_returns_true() throws IOException {
+        File fileToDelete = tmpFolder.newFile(deleteFilename);
+
+        Boolean result = fixture.deleteIfExists(fileToDelete.getPath());
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testDeleteIfExists_failing_delete_throws_exception() throws IOException {
+        File fileToDelete = tmpFolder.newFile(deleteFilename);
+
+        //Forces file.delete in fixture.delete() to fail so expected exception will be thrown
+        new RandomAccessFile(fileToDelete, "r");
+
+        Throwable thrown = catchThrowable(() -> fixture.deleteIfExists(fileToDelete.getPath()));
+
+        assertThat(thrown)
+                .isInstanceOf(SlimFixtureException.class)
+                .hasMessageContaining("Unable to delete file");
     }
 
     @Test
@@ -116,6 +144,7 @@ public class FileFixtureTest {
         }
     }
 
+    //
     @Test
     public void testAppendContainingOnSameline() {
         try {
@@ -125,7 +154,7 @@ public class FileFixtureTest {
             String expectedPath = getExpectedUrlPath();
             assertEquals("<a href=\"file:" + expectedPath + "\" target=\"_blank\">" + copyFilename + "</a>", res);
             assertEquals("# Expected as first line of text.\n# Expected as 2nd line of text.Third line",
-                    fixture.textIn(copyFilename));
+                    new LineEndingHelper().convertEndingsTo(fixture.textIn(copyFilename), "\n"));
         } catch (IOException ioe) {
             fail("Should not happen: " + ioe.getMessage());
         }
@@ -135,18 +164,26 @@ public class FileFixtureTest {
         String path = testResourcesDir + copyFilename;
         String unixStylePath = path.replace(File.separatorChar, '/');
         if (!unixStylePath.startsWith("/")) {
-            unixStylePath = "/" + unixStylePath ;
+            unixStylePath = "/" + unixStylePath;
         }
         return unixStylePath;
     }
 
     @Test
-    public void testDelete() {
-        try {
-            fixture.setDirectory(testResourcesDir);
-            fixture.delete(copyFilename);
-        } catch (SlimFixtureException sfe) {
-            fail("Should not happen: " + sfe.getMessage());
-        }
+    public void testDelete_existing() throws IOException {
+        File fileToDelete = tmpFolder.newFile(deleteFilename);
+
+        fixture.delete(fileToDelete.getPath());
+
+        assertThat(fileToDelete.exists()).isFalse();
+    }
+
+    @Test
+    public void testDelete_non_existing_throws_exception() throws IOException {
+        String nonExistingFilePath = getTmpFolderPath() + "i-do-not-exist.lol";
+
+        Throwable thrown = catchThrowable(() -> fixture.delete(nonExistingFilePath));
+
+        assertThat(thrown).isInstanceOf(SlimFixtureException.class);
     }
 }
